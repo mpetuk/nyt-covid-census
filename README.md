2b5162463355ed0fffaeca7949e278e41ce42e00
mpetuk <mpetuk@users.noreply.github.com>
2020-11-19 03:41:16 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-# nyt-covid-census
+# Daily and Weekly Updates of Nationwide Infection Counts by County
+
+This repo is designed to prepare daily and weekly COVID-19 summaries by county using the two data sources below:
+
+* New York Times COVID-19 Data ([source](https://github.com/nytimes/covid-19-data/blob/master/README.md))
+
+* 2019 Population Estimate Data 2019 Population Estimate Data ([source](https://www.census.gov/data/datasets/time-series/demo/popest/2010s-counties-total.html))
+
+Everything is handled by _covid_update_county_daily.py_ script, i.e.:
+<ol>
+<li>Downloading COVID and Census data 
+<ul>
+<li>Census data, being practically static, is downloaded once and stored in ./census folder until next year data becomes available</li>
+</ul>
+<li>Combining them by appending 2019 (most recent) county population estimates to COVID-19 data</li>
+<li>Summarizing data by day and FIPS as well as by week and FIPS</li>
+<li>Outputing summary files to ./output/daily and ./output/weekly folders, respectively</li>
+</ol>
+<p>
+For every FIPS code<sup>1</sup> and date<sup>2</sup>, the following metrics are provided: population, daily/weekly
+cases, daily/weekly deaths, cumulative cases to date, and cumulative death counts to
+date.<br>
+
+The script is designed to be run daily and provide the latest COVID-19 data available at time of execution. 
+At the very first execution, the script is designed to create folders structure referred to above.
+</p>
+<sup>1</sup> <sub>there are missing FIPS in COVID-19 data as well as FIPS that don't match to Census data. These are currently omitted from the output with the exception of New York City</sub>
+
+<sup>2</sup> <sub>every date available in COVID-19 data. Gaps in dates are possible and have not been addressed at this point </sub>
+
+## Learnings & thoughts about the data sources:
+<ul>
+<li>COVID-19:</li>
+<ul>
+<li>contains 742,008 records</li>
+<li>contains data on 3,217 FIPS</li>
+<li>unlike Census data, contains data for US territories</li>
+<ul>
+<li>FIPS > 60000</li>
+</ul>
+<li>contains records with missing FIPS (7,068 or ~1%)</li>
+<ul>
+<li> every state has at least one such record</li>
+</ul>
+<ul>
+<li>there are 4 distinct counties with missing FIPS:'New York City', 'Unknown', 'Kansas City', and 'Joplin' </li>
+<ul>
+<li> 'New York City' is not a county; instead it comprises of 5 counties ("36005","36047","36061","36081","36085": none of these are in the Covid file) - this has been used to fill missing population for 'New York City' records</li>
+<li> 'Joplin' is not a county; it is the largest city in Jasper County, Missouri</li>
+<li> 'Kansas City' is not a county; most of it lies within Jackson County, Missouri</li>
+<li> there are records for both, Jasper and Jackson counties - will need consolidation</li>
+</li>
+</ul>
+</ul>
+<li>contains records with county == 'Uknown'</li>
+<ul>
+<li> all of such records have missing FIPS</li>
+</ul>
+<li>contains data for 302 days in the range of (2020-01-21 and 2020-11-17 as of 2020-11-18</li>
+<ul>
+<li> only one county has data for all 302 days (FIPS ==53061 or Snohomish County in Washington)</li>
+<li> average number of reporting days per county is 228</li>
+<li> one county, Loving in Texas (FIPS == 48301, which is Blanco County in Census file), has only one record: for 2020-11-17</li>
+</ul>
+<li> county names for the same FIPS can be different from the Census ones</li>
+</ul>
+<li>Census:</li>
+<ul>
+<li>carries records for continental US only</li>
+<li>contains 3,142 records, one for each FIPS (75 FIPS less than COVID-19 data)</li>
+<li>contains 164 columns, with 16 groups of yearly metrics</li>
+<li>need to concatinate state and county codes to get FIPS</li>
+</ul>
+
+
+## How you sanity checked your output
+<ul>
+<li>sum of county's daily/weekly metrics should be equal to the most recent cumulative ones: compare cases and deaths summed to date and fips level with cumulative cases and deaths retrieved from the records with maximum date</li>
+<li>cumulative metrics should not decrease overtime within each county - ranks (without ties (method = first)) based on date and cumulative metrics should coincide</li>
+<li>weekly summary file should be smaller than daily by the factor of ~7</li>
+</ul>
+
+## Things you would add given more time
+
+<ul>
+<li>add logging</li>
+<li>reconcile the rest of the records with FIPS missing (and county is not 'Uknown') or not matching Census (US territories)</li>
+<ul>
+<li>get population for US territories' counties</li>
+</ul>
+<li>add downloading of a new Census file based on current date and the date when next year's data becomes available (once a year)</li>
+<li>choose population metric based on current date </li>
+<li>process only net new/delta records from COVID-19 data and change structure to update existing report with new data points</l>
+<li>consider adding records for missing dates so that each county has the same number of records - to help with dashboards, charts, and other representations of time series data</l>
+</ul>
+
+
---
+++ covid_update_county_daily.py
@@ -0,0 +1,132 @@
+import numpy as np
+import pandas as pd
+import datetime
+import os
+import urllib.request
+import sys
+
+# New York Times Covid-19 data
+# Updated daily: new rows added
+
+url_covid = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
+
+# Census data: continental US population by county 
+# Updated once a year: new columns added
+
+url_census= 'https://www2.census.gov/programs-surveys/popest/datasets/2010-2019/counties/totals/co-est2019-alldata.csv'
+
+
+# Download census file locally unless already downloaded:
+
+CENSUS_URL = "https://www2.census.gov/programs-surveys/popest/datasets/2010-2019/counties/totals/co-est2019-alldata.csv"
+CENSUS_PATH = os.path.join(os.path.dirname(os.path.realpath('__file__')),"census")
+
+def fetch_census_data(census_url=CENSUS_URL, census_path=CENSUS_PATH):
+    if not os.path.isdir(census_path):
+        os.makedirs(census_path)
+    file_path = os.path.join(census_path, "counties_est2019.csv")
+    if not os.path.isfile(file_path):
+        print('Downloading census file')
+        urllib.request.urlretrieve(census_url, file_path)
+    else:
+        print('Census file exists; skipping download')
+
+fetch_census_data()
+
+# Load columns of interest from  census file into pandas dataframe
+# Note: columns 3 and 4 are 'state' and 'county' 2-digit and 3-digit codes, respectively, with leading zeros where applicable
+
+census_df = pd.read_csv(os.path.join(CENSUS_PATH, "counties_est2019.csv")
+                        , header=None
+                        , skiprows=1
+                        , encoding='latin-1'
+                        , usecols =[0,3,4,18]
+                        , dtype = {3:str,4:str,18:np.int64}) 
+
+census_df.columns = ['sumlev','state','county','popestimate2019']
+
+# Column 'sumlev' identifies summary level: 50 == county, 40 == state
+census_counties_df = pd.DataFrame(census_df[census_df['sumlev']==50])
+
+# Create 5-digit long fips column
+census_counties_df['fips'] = census_counties_df['state'] + census_counties_df['county']
+
+
+# Download daily NYT Covid-19 data
+covid_df = pd.read_csv(url_covid
+                       , dtype = {'date': str, 'fips':str}
+                       , parse_dates = ['date'])
+
+# Merge Covid data with Census data on fips with Covid data as a driver (hence left join)
+# indicator = True adds column '_merge' to the dataframe - useful for quantifying mis/matches 
+combo_df = covid_df.merge(census_counties_df[['fips','popestimate2019']]
+                          , how = 'left'
+                          , on = 'fips'
+                          , indicator = True)
+
+
+
+# In the  Covid data there is a county 'New York City' - not a real county --> doesn't match to Census data
+# New York City comprises of 5 counties (none of them are present in NYT Covid data)
+# Filling missing population for 'New York City' with sum of populations of 5 of its counties, 8,336,817
+# census_counties_df[census_counties_df['fips'].isin(["36005","36047","36061","36081","36085"])].popestimate2019.sum()
+# 8336817
+
+combo_df.loc[(combo_df['popestimate2019'].isna()) & (combo_df['county']=='New York City'), 'popestimate2019'] = 8336817
+
+# Dropping records for the rest of the unmatched counties from Covid data for now 
+combo_df = combo_df[(combo_df['_merge']!='left_only')|(combo_df['county']=='New York City')]
+
+# Filling missing fips for 'New York City' with a made-up value to retain it in the summary data
+combo_df.loc[combo_df['fips'].isna(),'fips'] =  'nycny'
+
+
+# Add column capturing start date of the reporting week
+combo_df['week_start'] = combo_df['date'].dt.to_period('W').apply(lambda r: r.start_time)
+
+# Create timestamp to be used in the output file name
+timestamp = datetime.datetime.today().strftime('%Y%m%d')
+
+# Create weekly and daily summary outputs:
+
+for i in ['date','week_start']:
+
+    # Create dataframe with cumulative sums for cases and deaths to date
+    cumulative_df = combo_df[[i,'fips','cases','deaths']]\
+                            .groupby(['fips',i], sort=False)\
+                            .sum()\
+                            .groupby(by='fips',sort=False)\
+                            .cumsum()\
+                            .reset_index()
+
+    # Rename columns before merge
+    cumulative_df.columns = ['fips',i,'cumulative_cases','cumulative_deaths']
+    
+    if i == 'week_start':
+        sumlev = 'weekly'
+        
+        summary_df =  combo_df[[i,'fips','county','state','popestimate2019','cases','deaths']]\
+                                   .groupby([i,'fips','county','state','popestimate2019'])\
+                                   .agg({'cases':'sum','deaths':'sum'})\
+                                   .reset_index()
+    else:
+        sumlev = 'daily'
+        summary_df = pd.DataFrame(combo_df)
+   
+    # Add cumulative metrics back to the combined covid and census dataframe
+    summary_df = summary_df.merge(cumulative_df, how = 'inner', on = [i,'fips'])
+   
+    # Add column capturing load date of the data
+    summary_df['load_date'] = pd.to_datetime('today').date()
+        
+
+    # Output dataframe as csv to output folder
+    output_path = os.path.join(os.path.dirname(os.path.realpath('__file__')),"output", sumlev)
+    if not os.path.isdir(output_path):
+       os.makedirs(output_path)
+
+    outfile_path = os.path.join(output_path, 'nyt_covid_' + timestamp + '.csv')
+
+    summary_df.to_csv(open(outfile_path,'w'), index=False)
+
+
